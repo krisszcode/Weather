39df287bf43448755d2a6b0607807366f8c12840
krisszcode <nagy.ace.krisztian@gmail.com>
2021-01-13 13:46:10 +0100

    gets the location of user and displays an icon for loading

--- src/components/Weathercard.tsx
+++ src/components/Weathercard.tsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
 
 import axios from 'axios'
-import logo from '../images/logo.jpg'
 
-interface ICity{
-  cityname: string;
-}
 interface IPost {
   temperature: number;
   weather: string;
@@ -13,25 +9,51 @@ interface IPost {
 }
 
 
-
 const css = require('./Weathercard.css')
+
+
 
 class WeatherCard extends Component {
 
   state = {
     city: "",
+    cityToDisplay: "",
+    latitude:0,
+    longitude: 0,
+    pictureToDisplay: "",
     posts: {temperature: 0,
       weather: "",
       feelsLike: 0,
     }}
 
-  componentWillMount(){
-    this.setState({
-      city: "Miskolc"
-    }, this.getLocationData)
-    console.log(this.state.city)
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude:position.coords.longitude
+        },() => this.getLocationDataByCoords(this.state.latitude,this.state.longitude))
 
+      },
+      (error) => {
+        console.error("Error Code = " + error.code + " - " + error.message);
+        this.setState({
+          city: "Budapest",
+          cityToDisplay: "Budapest"
+        }, this.getLocationDataByName)
+      }
+    );
   }
+
+  // componentWillMount(){
+  //   this.setState({
+  //     city: "Győr",
+  //     cityToDisplay: "Győr"
+  //   }, this.getLocationDataByName)
+  // }
+
 
   defaultPost: IPost = {temperature: 0, weather: "", feelsLike: 0 }
 
@@ -44,42 +66,84 @@ class WeatherCard extends Component {
 
   onSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault()
-
+    this.getLocationDataByName()
   }
 
-
-  getLocationData(){
+  getLocationDataByCoords(latitude:Number,longitude:Number){
     axios
-      .get<any>(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+    .get<IPost>(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+    .then((response: any) => {
+      const newPost :IPost =
+      {temperature: Number(response.data.main.temp),
+        weather: String(response.data.weather[0].main),
+        feelsLike: Number(response.data.main.feels_like)}
+        if(response.status === 200){
+          this.setState({
+            posts: newPost,
+            pictureToDisplay: `/weatherIMG/${response.data.weather[0].main}.jpg`,
+            cityToDisplay : response.data.name
+          }
+
+          ,()=>{
+            console.log(this.state.posts)
+            console.log(this.state.pictureToDisplay)
+          })
+        }
+  })
+  }
+
+  getLocationDataByName(){
+    axios
+      .get<IPost>(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
       .then((response: any) => {
         const newPost :IPost =
         {temperature: Number(response.data.main.temp),
           weather: String(response.data.weather[0].main),
           feelsLike: Number(response.data.main.feels_like)}
-        this.setState({
-          posts: newPost
-        },()=>{
-          console.log(this.state.posts)
-        })
-    });
+          if(response.status === 200){
+            this.setState({
+              posts: newPost,
+              pictureToDisplay: `/weatherIMG/${response.data.weather[0].main}.jpg`,
+              cityToDisplay : this.state.city
+            }
+
+            ,()=>{
+              console.log(this.state.posts)
+              console.log(this.state.pictureToDisplay)
+            })
+          }
+    })
   }
 
-
     render() {
+
     return (
+
       <div>
-        <div className="card">
-          <img className="card-img-top" src={logo} alt="Card image"/>
-          <div className="card-body">
-            <form onSubmit={this.onSubmit}>
-              <h4 className="card-title">John Doe</h4>
-              <p className="card-text">Some example text.</p>
-              <input type="text" onChange={this.onChangeText}></input>
-              <button>gomb</button>
-            </form>
+        {this.state.cityToDisplay!== "" ?
+          <div className="card">
+          <div>
+            <img className="card-img-top" src={this.state.pictureToDisplay} alt={this.state.posts.weather}/>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <h4 className="card-title">{this.state.cityToDisplay}</h4>
+                <p className="card-text">{this.state.posts.weather}</p>
+                <p className="card-text">{this.state.posts.temperature} ℃</p>
+                <p className="card-text">{this.state.posts.feelsLike} ℃</p>
+                <input type="text" onChange={this.onChangeText}></input>
+                <button>gomb</button>
+              </form>
+            </div>
+            </div>
+          </div> :
+          <div className="d-flex justify-content-center">
+            <div className="spinner-border" role="status">
+            </div>
+            <p>Please enable location</p>
           </div>
-        </div>
+        }
       </div>
+
     )
   }
 }
